b4e28525d5e6a3f14950bdfdc0bd26b92c1ba186
hadleyking <hadley_king@gwu.edu>
2024-07-08 11:34:15 -0400

    Update config files
    Changes to be committed:
    	modified:   admin/gregor.conf
    	modified:   server/config/urls.py

--- server/config/urls.py
+++ server/config/urls.py
@@ -25,14 +25,14 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("metadata/", include("metadata.urls")),
-    path("experiments/", include("experiments.urls")),
-    path("search/", include("search.urls")),
+    path("api/metadata/", include("metadata.urls")),
+    path("api/experiments/", include("experiments.urls")),
+    path("api/search/", include("search.urls")),
     path(
-        "swagger/",
+        "api/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("admin/", admin.site.urls),
+    path("api/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/admin/", admin.site.urls),
 ]
