f33eb011ca8f8d878b536aa8adb8865cd2e898bf
hadleyking <hadley_king@gwu.edu>
2024-07-25 11:45:16 -0400

    Add AnVIL upload data API
    Changes to be committed:
    	modified:   server/config/selectors.py
    	modified:   server/search/apis.py
    	modified:   server/search/selectors.py
    	modified:   server/search/urls.py

--- server/search/urls.py
+++ server/search/urls.py
@@ -1,9 +1,10 @@
 # search/urls.py
 
 from django.urls import path
-from search.apis import SearchTablesAPI, TestConnection
+from search.apis import SearchTablesAPI, TestConnection, DounlaodTablesAPI
 
 urlpatterns = [
+    path("get_anvil_tables/", DounlaodTablesAPI.as_view()),
+    path("test/", TestConnection.as_view(), name="test"),
     path("<str:model_name>/", SearchTablesAPI.as_view(), name="general_search"),
-    path("test", TestConnection.as_view(), name="test"),
 ]
