04597cece1c077ba7469f67001cfa5478be5ae95
HadleyKing <charles.hadley.king@gmail.com>
2025-01-28 11:50:22 -0500

    Genetic Findings table handling added.
    Changes to be committed:
    	new file:   client/public/GREGoR_Final_Logo.png
    	modified:   client/src/components/FormDialogue.js
    	modified:   client/src/components/TableForm.js
    	deleted:    client/src/components/TableFormik.js
    	modified:   client/src/components/Uploader.js
    	modified:   client/src/pages/GregorTables.js
    	modified:   client/src/pages/Home.js
    	modified:   client/src/services/data.service.js
    	modified:   client/src/slices/dataSlice.js

--- client/src/components/FormDialogue.js
+++ client/src/components/FormDialogue.js
@@ -1,6 +1,6 @@
 // src/components/FormDialogue.js
 
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {
   Dialog,
@@ -40,10 +40,16 @@ const jsonSchemaToYup = (jsonSchema) => {
         if (value.minLength) validator = validator.min(value.minLength);
         if (value.maxLength) validator = validator.max(value.maxLength);
       } else if (value.type === 'integer') {
-        validator = Yup.number().integer();
+        validator = Yup.number().integer()
+          .typeError(`${key} must be a valid integer`) // Error for non-integer values
         if (value.minimum) validator = validator.min(value.minimum);
         if (value.maximum) validator = validator.max(value.maximum);
-       }else if (value.type === 'array' && value.items?.enum) {
+       } else if (value.type === 'number') {
+        validator = Yup.number()
+          .typeError(`${key} must be a valid number`) // Error for non-numeric values
+        if (value.minimum) validator = validator.min(value.minimum);
+        if (value.maximum) validator = validator.max(value.maximum);
+      } else if (value.type === 'array' && value.items?.enum) {
         validator = Yup.array()
           .of(Yup.string().oneOf(value.items.enum))
           .min(1, `${key} must have at least one selected value`);
@@ -60,29 +66,37 @@ const jsonSchemaToYup = (jsonSchema) => {
     return Yup.object().shape(yupSchema);
   };
 
-const DialogForm = ({ open, onClose, schema, selectedRow, rowID }) => {
-    const dispatch = useDispatch();
-    const token = useSelector((state) => state.account.user?.access_token);
-    const yupSchema = React.useMemo(() => jsonSchemaToYup(schema), [schema]);
+const DialogForm = ({ open, onClose, schema, selectedRow, rowID, identifier }) => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.account.user?.access_token);
+  const yupSchema = React.useMemo(() => jsonSchemaToYup(schema), [schema]);
+  
+  // Default initialization of selectedRow
+  const initialValues = React.useMemo(() => {
+      if (!selectedRow) return {};
+
+      const defaultValues = {};
+      Object.entries(schema.properties).forEach(([key, fieldSchema]) => {
+        if (fieldSchema.type === 'array') {
+          defaultValues[key] = Array.isArray(selectedRow?.[key]) ? selectedRow[key] : [];
+        } else if (fieldSchema.type === 'integer') {
+          defaultValues[key] = selectedRow?.[key] ?? null; // Use null for missing integer values
+        } else {
+          defaultValues[key] = selectedRow?.[key] ?? ''; // Default to an empty string for other types
+        }
+      });
     
-    // Default initialization of selectedRow
-    const initialValues = React.useMemo(() => {
-        if (!selectedRow) return {};
-
-        const defaultValues = {};
-        Object.entries(schema.properties).forEach(([key, fieldSchema]) => {
-            if (fieldSchema.type === "array" && fieldSchema.items?.type === "string") {
-                // Initialize arrays with empty array if not provided
-                defaultValues[key] = Array.isArray(selectedRow[key]) ? selectedRow[key] : [];
-            } else {
-                // Use provided value or an empty string as default
-                defaultValues[key] = selectedRow[key] ?? "";
-            }
-        });
-        return { ...defaultValues, ...selectedRow }; // Merge with the actual values
+      return defaultValues;
     }, [selectedRow, schema]);
+    
 
-    return (
+  const [initialFieldValues, setInitialFieldValues] = useState({});
+
+  useEffect(() => {
+    setInitialFieldValues(initialValues);
+  }, [initialValues]);
+  
+  return (
     <Dialog open={open} onClose={onClose} className="dialog-form-container">
       <DialogTitle className="dialog-form-title">{schema.title || "Form" } Details</DialogTitle>
       <DialogContent className="dialog-form-content">
@@ -91,19 +105,20 @@ const DialogForm = ({ open, onClose, schema, selectedRow, rowID }) => {
             initialValues={initialValues}
             validationSchema={yupSchema}
             onSubmit={async (values) => {
-                console.log('Form Submitted', rowID, values);
-                try {
-                  const result = await dispatch(updateTable({table:rowID, data: values, token:token}));
-                  if (result.meta.requestStatus === 'fulfilled') {
+              try {
+                // console.log(JSON.stringify(values))
+                const result = dispatch(updateTable({table:rowID, data: values, token:token}));
+                if (result.meta.requestStatus === 'fulfilled') {
                   onClose();
-                  } else {
-                    console.log('Form error', result.error);
-                    onClose();
-                  }
-                } catch (error) {
-                  console.log('Form error', error);
+                } else {
+                  console.log('Form error', result.error);
                   onClose();
                 }
+              } catch (error) {
+                console.log('Form error', error);
+                onClose();
+              }
+              // console.log('Form Submitted', rowID, values);
               }}
           >
             {({ values, handleChange, handleBlur, touched, errors }) => (
@@ -127,6 +142,7 @@ const DialogForm = ({ open, onClose, schema, selectedRow, rowID }) => {
                         </Tooltip>
                       </Grid>
                       <Grid item xs>
+                        {/* Handle arrays with enum => Checkbox */}
                         {fieldSchema.type === 'array' && fieldSchema.items?.enum ? (
                           <FormGroup>
                             {fieldSchema.items.enum.map((option) => (
@@ -165,67 +181,69 @@ const DialogForm = ({ open, onClose, schema, selectedRow, rowID }) => {
                             )}
                           </FormGroup>
                         ) : fieldSchema.type === 'array' && fieldSchema.items?.type === 'string' ? (
-                        <FieldArray name={key}>
-                          {(arrayHelpers) => (
+                          <FieldArray name={key}>
+                            {/* Handle arrays with string => List of textbox */}
+                            {(arrayHelpers) => (
                               <div>
-                              {Array.isArray(values[key]) && values[key]?.map((item, index) => (
-                                <Grid container spacing={1} alignItems="center" key={index}>
-                                  <Grid item xs={10}>
-                                    <TextField
-                                      name={`${key}.${index}`}
-                                      value={item}
-                                      onChange={handleChange}
-                                      onBlur={handleBlur}
-                                      fullWidth
-                                      margin="normal"
-                                      error={touched[key]?.[index] && Boolean(errors[key]?.[index])}
-                                      helperText={touched[key]?.[index] && errors[key]?.[index]}
-                                      label={`Item ${index + 1}`}
-                                    />
+                                {Array.isArray(values[key]) && values[key]?.map((item, index) => (
+                                  <Grid container spacing={1} alignItems="center" key={index}>
+                                    <Grid item xs={10}>
+                                      <TextField
+                                        name={`${key}.${index}`}
+                                        value={item}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        fullWidth
+                                        margin="normal"
+                                        error={touched[key]?.[index] && Boolean(errors[key]?.[index])}
+                                        helperText={touched[key]?.[index] && errors[key]?.[index]}
+                                        label={`Item ${index + 1}`}
+                                      />
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                      <Button
+                                        type="button"
+                                        onClick={() => arrayHelpers.remove(index)}
+                                        color="secondary"
+                                      >
+                                        <RemoveCircleIcon />
+                                      </Button>
+                                    </Grid>
                                   </Grid>
-                                  <Grid item xs={2}>
-                                    <Button
-                                      type="button"
-                                      onClick={() => arrayHelpers.remove(index)}
-                                      color="secondary"
-                                    >
-                                      <RemoveCircleIcon />
-                                    </Button>
-                                  </Grid>
-                                </Grid>
-                              ))}
-                              <Button
-                                type="button"
-                                onClick={() => arrayHelpers.push([""])} // Add a new empty string to the array
-                                color="primary"
-                                startIcon={<AddCircleIcon />}
+                                ))}
+                                <Button
+                                  type="button"
+                                  onClick={() => arrayHelpers.push([""])} // Add a new empty string to the array
+                                  color="primary"
+                                  startIcon={<AddCircleIcon />}
+                                >
+                                  Add Item
+                                </Button>
+                              </div>
+                            )}
+                          </FieldArray>
+                        ) : fieldSchema.enum ? (
+                          <FormControl fullWidth margin="normal">
+                            {/* // Handle dropdowns */}
+                            <InputLabel id={`${key}-label`}>{key}</InputLabel>
+                              <Select
+                                labelId={`${key}-label`}
+                                id={key}
+                                name={key}
+                                value={values[key] || ''}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                error={touched[key] && Boolean(errors[key])}
                               >
-                                Add Item
-                              </Button>
-                            </div>
-                          )}
-                        </FieldArray>
-                      ) : fieldSchema.enum ? (
-                        <FormControl fullWidth margin="normal">
-                          <InputLabel id={`${key}-label`}>{key}</InputLabel>
-                            <Select
-                              labelId={`${key}-label`}
-                              id={key}
-                              name={key}
-                              value={values[key] || ''}
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              error={touched[key] && Boolean(errors[key])}
-                            >
-                            {fieldSchema.enum.map((option) => (
-                              <MenuItem key={option} value={option}>
-                                {option}
-                              </MenuItem>
-                            ))}
-                            </Select>
-                          <FormHelperText>{touched[key] && errors[key]}</FormHelperText>
-                        </FormControl>
-                        ) : (
+                              {fieldSchema.enum.map((option) => (
+                                <MenuItem key={option} value={option}>
+                                  {option}
+                                </MenuItem>
+                              ))}
+                              </Select>
+                            <FormHelperText>{touched[key] && errors[key]}</FormHelperText>
+                          </FormControl>
+                        ) : fieldSchema.type === 'number' ? (
                           <TextField
                             label={key}
                             name={key}
@@ -233,13 +251,47 @@ const DialogForm = ({ open, onClose, schema, selectedRow, rowID }) => {
                             onChange={handleChange}
                             onBlur={handleBlur}
                             fullWidth
-                            disabled={key === rowID && values[key] !== ''}
+                            margin="normal"
+                            error={touched[key] && Boolean(errors[key])}
+                            helperText={touched[key] && errors[key]}
+                          />
+                        ) : fieldSchema.type === 'integer' ? (
+                          <TextField
+                          label={key}
+                          name={key}
+                          type="number" // Use "number" type for integers
+                          inputProps={{ step: 1 }} // Ensures only integers can be entered
+                          value={values[key] || ''}
+                          onChange={(e) => {
+                            const value = e.target.value;
+                            handleChange({
+                              target: {
+                                name: key,
+                                value: value === '' ? '' : parseInt(value, 10), // Parse as integer
+                              },
+                            });
+                          }}
+                          onBlur={handleBlur}
+                          fullWidth
+                          margin="normal"
+                          error={touched[key] && Boolean(errors[key])}
+                          helperText={touched[key] && errors[key]}
+                        />
+                        ) : (
+                          <TextField
+                            label={key}// All other values
+                            name={key}
+                            value={values[key] || ''}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            fullWidth
+                            disabled={initialFieldValues[key] !== '' && identifier == key}
                             margin="normal"
                             error={touched[key] && Boolean(errors[key])}
                             helperText={touched[key] && errors[key]}
                           />
                         )}
-                        </Grid>
+                      </Grid>
                     </Grid>
                   );
                 })}
--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -1,14 +1,13 @@
 
 import { useDispatch, useSelector } from 'react-redux';
 import CSVReader from 'react-csv-reader'
-import { setJsonData, submitParticipant } from '../slices/dataSlice';
 import TableForm from './TableForm';
 
 export const Uploader = () => {
   const dispatch = useDispatch();
   const tableData = useSelector(state => state.data['jsonData'])
   const handleCsvUpload = (sheet) => {
-    dispatch(setJsonData(sheet));
+    // dispatch(setJsonData(sheet));
   };
 
   return (
