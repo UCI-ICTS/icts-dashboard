65311b81b9cbc8145deb8988695b52f41325de0b
HadleyKing <charles.hadley.king@gmail.com>
2025-04-11 14:12:30 -0400

    Notification timeout
    Fix #84
    
    Changes to be committed:
    	modified:   client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
+import { message } from 'antd'; // ← Import message from antd
+
+// Set global message duration (in seconds)
+message.config({
+  duration: 5, // e.g. show messages for 5 seconds instead of 2
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
