fbe60564ea63033bfa27cb1941f69988c77c6a5c
HadleyKing <charles.hadley.king@gmail.com>
2025-05-12 09:01:25 -0400

    Fix Phenyotype tests
    Changes to be committed:
    	modified:   server/tests/test_apis/test_phenotype_apis.py

--- server/tests/test_apis/test_phenotype_apis.py
+++ server/tests/test_apis/test_phenotype_apis.py
@@ -5,17 +5,21 @@ from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
 
+
 class APITestCaseWithAuth(APITestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
+    fixtures = ["tests/fixtures/test_fixture.json"]
 
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
         self.client.force_authenticate(user=self.user)
+
 
 class CreatePhenotypeAPITest(APITestCaseWithAuth):
     def test_create_analyte_api(self):
-        url = "/api/metadata/create_phenotypes/"
+        url = "/api/metadata/create_phenotype/"
         part1 = {  # Valid submission
             "phenotype_id": "1.10",
             "participant_id": "GREGoR_test-001-001-0",
@@ -25,7 +29,7 @@ class CreatePhenotypeAPITest(APITestCaseWithAuth):
             "additional_details": "gross motor delay",
             "onset_age_range": "HP:0011463",
             "additional_modifiers": [],
-            "syndromic": "non-syndromic"
+            "syndromic": "non-syndromic",
         }
         part2 = {  # Valid submission 2
             "phenotype_id": "1.11",
@@ -36,7 +40,7 @@ class CreatePhenotypeAPITest(APITestCaseWithAuth):
             "additional_details": "Dysgenesis of the cerebellar vermis",
             "onset_age_range": "HP:0011463",
             "additional_modifiers": [],
-            "syndromic": "non-syndromic"
+            "syndromic": "non-syndromic",
         }
         part3 = {  # Invalid submission; missing ontology
             "phenotype_id": "1.12",
@@ -47,26 +51,27 @@ class CreatePhenotypeAPITest(APITestCaseWithAuth):
             "additional_details": "Myelopathy",
             "onset_age_range": "HP:0011463",
             "additional_modifiers": [],
-            "syndromic": "non-syndromic"
+            "syndromic": "non-syndromic",
         }
-        response_200 = self.client.post(url, [part1], format='json')
-        response_207 = self.client.post(url, [part2, part3], format='json')
-        response_400 = self.client.post(url, [part3], format='json')
+        response_200 = self.client.post(url, [part1], format="json")
+        response_207 = self.client.post(url, [part2, part3], format="json")
+        response_400 = self.client.post(url, [part3], format="json")
         self.assertEqual(response_200.status_code, status.HTTP_200_OK)
         self.assertEqual(response_207.status_code, status.HTTP_207_MULTI_STATUS)
         self.assertEqual(response_207.data[0]["request_status"], "CREATED")
         self.assertEqual(response_207.data[1]["request_status"], "BAD REQUEST")
         self.assertEqual(response_400.status_code, status.HTTP_400_BAD_REQUEST)
 
+
 class ReadPhenotypeAPITest(APITestCaseWithAuth):
     def test_read_phenotype_success(self):
-        url1 = "/api/metadata/read_phenotypes/?ids=1.2,1.3"
-        url2 = "/api/metadata/read_phenotypes/?ids=1.2,1.3,1.99"
-        url3 = "/api/metadata/read_phenotypes/?ids=1.99,1.100"
+        url1 = "/api/metadata/read_phenotype/?ids=1.2,1.3"
+        url2 = "/api/metadata/read_phenotype/?ids=1.2,1.3,1.99"
+        url3 = "/api/metadata/read_phenotype/?ids=1.99,1.100"
 
-        response_200 = self.client.get(url1, format='json')
-        response_207 = self.client.get(url2, format='json')
-        response_400 = self.client.get(url3, format='json')
+        response_200 = self.client.get(url1, format="json")
+        response_207 = self.client.get(url2, format="json")
+        response_400 = self.client.get(url3, format="json")
         self.assertEqual(response_200.status_code, status.HTTP_200_OK)
         self.assertEqual(response_207.status_code, status.HTTP_207_MULTI_STATUS)
         self.assertEqual(response_207.data[0]["request_status"], "SUCCESS")
@@ -74,9 +79,10 @@ class ReadPhenotypeAPITest(APITestCaseWithAuth):
         self.assertEqual(response_207.data[2]["request_status"], "NOT FOUND")
         self.assertEqual(response_400.status_code, status.HTTP_400_BAD_REQUEST)
 
+
 class UpdatePhenotypeAPITest(APITestCaseWithAuth):
     def test_update_phenotype_api(self):
-        url = "/api/metadata/update_phenotypes/"
+        url = "/api/metadata/update_phenotype/"
         part1 = {  # Valid submission
             "phenotype_id": "1.2",
             "participant_id": "GREGoR_test-001-001-0",
@@ -86,7 +92,7 @@ class UpdatePhenotypeAPITest(APITestCaseWithAuth):
             "additional_details": "gross motor delay",
             "onset_age_range": "HP:0011463",
             "additional_modifiers": ["HP:0025292"],
-            "syndromic": "non-syndromic"
+            "syndromic": "non-syndromic",
         }
         part2 = {  # Invalid submission; invalid additional_modifiers
             "phenotype_id": "1.3",
@@ -97,20 +103,22 @@ class UpdatePhenotypeAPITest(APITestCaseWithAuth):
             "additional_details": "migraines",
             "onset_age_range": "HP:0003621",
             "additional_modifiers": ["feeding difficulties"],
-            "syndromic": "non-syndromic"
+            "syndromic": "non-syndromic",
         }
-        response_200 = self.client.post(url, [part1], format='json')
-        response_207 = self.client.post(url, [part1, part2], format='json')
-        response_400 = self.client.post(url, [part2], format='json')
+        response_200 = self.client.post(url, [part1], format="json")
+        response_207 = self.client.post(url, [part1, part2], format="json")
+        response_400 = self.client.post(url, [part2], format="json")
+
         self.assertEqual(response_200.status_code, status.HTTP_200_OK)
         self.assertEqual(response_207.status_code, status.HTTP_207_MULTI_STATUS)
         self.assertEqual(response_207.data[0]["request_status"], "UPDATED")
         self.assertEqual(response_207.data[1]["request_status"], "BAD REQUEST")
         self.assertEqual(response_400.status_code, status.HTTP_400_BAD_REQUEST)
 
+
 class DeletePhenotypeAPITest(APITestCaseWithAuth):
     def test_delete_phenotype(self):
-        url = "/api/metadata/delete_phenotypes/?ids=1.2"
-        response = self.client.delete(url, format='json')
+        url = "/api/metadata/delete_phenotype/?ids=1.2"
+        response = self.client.delete(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data[0]["request_status"], "DELETED")
