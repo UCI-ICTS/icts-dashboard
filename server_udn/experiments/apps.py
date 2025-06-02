5bf1025bb4e215090a055b4b62cc7f22bb0e4a8f
IvantheDugtrio <idedios@usc.edu>
2025-06-02 15:16:12 -0700

    Patched data converter to use create methods instead of create_or_update

--- server_udn/experiments/apps.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# experiments/admin.py
-
-from django.apps import AppConfig
-
-
-class Experiment(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "experiments"
