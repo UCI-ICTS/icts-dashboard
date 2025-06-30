f65973006982ef7f9cc1ffe516689e9ad1f2f472
HadleyKing <charles.hadley.king@gmail.com>
2025-06-30 12:13:45 -0400

    REACT_APP_APIDB changed back
    Changes to be committed:
    	modified:   client/src/api.js

--- client/src/api.js
+++ client/src/api.js
@@ -6,7 +6,7 @@ import {store} from "./store";
 import { logout } from "./slices/accountSlice";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || ""}/api/`,
+  baseURL: `${process.env.REACT_APP_APIDB || ""}/api/`,
   withCredentials: true,
 });
 
