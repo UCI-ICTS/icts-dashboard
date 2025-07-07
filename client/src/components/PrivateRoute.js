7db5889a94e3da4ae4ce16f933841125ec0f5163
Hadley King <HadleyKing@users.noreply.github.com>
2025-07-07 13:16:32 -0400

    Errormessage (#138)
    * Initial API changes client-side and sync with v1.8 data model
    
    * Initial API changes client-side and sync with v1.8 data model
    
    * Moved error handling to a new services file. Still testing antd messaging
    
    * Reverted biobank participant to participant_id for harmonization with other metadata tables and to fix issues with entering new biobank entries on the dashboard
    
    * Various metadata model fixes for odd behaviors when dealing with optional fields in the dashboard
    
    * Additional schema edits to sync with the v1.8 data model requirements
    
    ---------
    
    Co-authored-by: Ivan De Dios <idedios@usc.edu>

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,4 +1,4 @@
-// src/components/PrivateRout.js
+// src/components/PrivateRoute.js
 
 import React from "react";
 import { Navigate } from "react-router-dom";
--- client/src/services/data.service.js
+++ client/src/services/data.service.js
@@ -21,12 +21,15 @@ const getAuthHeaders = () => {
   };
 };
 
-const getAllTables = async () => {
-  const response = await axios.get(`${APIDB}api/search/get_all_tables/`, {
-    headers: getAuthHeaders(),
+
+const createFamily = async (data, token) => {
+  const response = await axios.post(APIDB + "api/metadata/family/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
   });
   return response;
-};
+}
 
 const createParticipant = async (data, token) => {
   const response = await axios.post(APIDB + "api/metadata/participant/create/", [
@@ -37,8 +40,151 @@ const createParticipant = async (data, token) => {
   return response;
 }
 
-const createFamily = async (data, token) => {
-  const response = await axios.post(APIDB + "api/metadata/family/create/", [
+const createPhenotype = async (data, token) => {
+  const response = await axios.post(APIDB + "api/metadata/phenotype/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createAnalyte = async (data, token) => {
+  const response = await axios.post(APIDB + "api/metadata/analyte/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createGeneticFindings = async (data, token) => {
+  const response = await axios.post(APIDB + "api/metadata/genetic_findings/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createBiobankEntries = async (data, token) => {
+  const response = await axios.post(APIDB + "api/metadata/biobank/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createExperiment = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/experiment/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createExpDnaShortRead = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/experiment_dna_short_read/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createExpRnaShortRead = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/experiment_rna_short_read/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createExpPacBio = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/experiment_pac_bio/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createExpNanopore = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/experiment_nanopore/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createAligned = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/alignment/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createAlnDnaShortRead = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/alignment_dna_short_read/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createAlnRnaShortRead = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/alignment_rna_short_read/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createAlnPacBio = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/alignment_pac_bio/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const createAlnNanopore = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/alignment_nanopore/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+
+const getAllTables = async () => {
+  const response = await axios.get(`${APIDB}api/search/get_all_tables/`, {
+    headers: getAuthHeaders(),
+  });
+  return response;
+}
+
+const createParticipant = async (data, token) => {
+  const response = await axios.post(APIDB + "api/metadata/participant/create/", [
+    data
+  ], {
+    headers: getAuthHeaders()
+  });
+  return response;
+}
+
+const updateAlnRnaShortRead = async (data, token) => {
+  const response = await axios.post(APIDB + "api/experiments/aligned_rna_short_read/update/", [
     data
   ], {
     headers: getAuthHeaders()
@@ -166,9 +312,6 @@ const dataService = {
   createDnaShortRead,
   createExperiment,
   createFamily,
-  createGeneticFindings,
-  createNanoPore,
-  createPacBio,
   createParticipant,
   createPhenotype,
   createRnaShortRead,
@@ -177,4 +320,4 @@ const dataService = {
   getAllTables
 }
 
-  export default dataService;
+export default dataService;
