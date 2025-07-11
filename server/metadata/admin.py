9c9d1f163eda388f73008ac5e10d88f372495818
HadleyKing <charles.hadley.king@gmail.com>
2025-07-11 14:01:03 -0400

    Enable partial updates via apis Changes to be committed: 	modified:   server/config/selectors.py 	modified:   server/experiments/apis.py 	modified:   server/experiments/services.py 	modified:   server/metadata/admin.py 	modified:   server/metadata/apis.py 	modified:   server/metadata/models.py 	modified:   server/metadata/services.py 	modified:   server/tests/test_apps/test_metadata/test_services.py 	modified:   server/utilities/data_converter.py

--- server/metadata/admin.py
+++ server/metadata/admin.py
@@ -1,5 +1,4 @@
-"""Metadata Admin Panel
-"""
+"""Metadata Admin Panel"""
 
 from django.contrib import admin
 from metadata.models import (
@@ -41,8 +40,31 @@ class GeneticFindingsAdmin(admin.ModelAdmin):
 class AnalyteAdmin(admin.ModelAdmin):
     list_display = ["analyte_id"]
 
+
+@admin.register(Biobank)
 class BiobankAdmin(admin.ModelAdmin):
-    list_display = ["biobank_id"]
+    list_display = [
+        "biobank_id",
+        "participant_id",
+        "display_analytes",
+        "display_experiments",
+        "status",
+        "collection_date",
+        "completed",
+    ]
+    search_fields = ["biobank_id", "participant__participant_id", "tube_barcode"]
+    list_filter = ["status", "completed", "collection_date"]
+    ordering = ["-collection_date"]
+
+    def display_analytes(self, obj):
+        return ", ".join(a.analyte_id for a in obj.child_analytes.all())
+
+    display_analytes.short_description = "Analytes"
+
+    def display_experiments(self, obj):
+        return ", ".join(e.experiment_id for e in obj.experiments.all())
+
+    display_experiments.short_description = "Experiments"
 
 
 admin.site.register(Participant, ParticipantAdmin)
@@ -52,4 +74,3 @@ admin.site.register(PmidId, PmidIdAdmin)
 admin.site.register(Phenotype, PhenotypeAdmin)
 admin.site.register(GeneticFindings, GeneticFindingsAdmin)
 admin.site.register(Analyte, AnalyteAdmin)
-admin.site.register(Biobank, BiobankAdmin)
--- server/tests/test_apps/test_metadata/test_services.py
+++ server/tests/test_apps/test_metadata/test_services.py
@@ -2,102 +2,20 @@
 # tests/test_metadata/test_services.py
 
 import json
+from io import StringIO
 from django.test import TestCase
-from metadata.models import Family, Participant, Phenotype, GeneticFindings, Analyte
-from metadata.selectors import get_analyte, genetic_findings_parser, participant_parser
+from metadata.models import Participant, Biobank, Analyte
+from django.core.management import call_command
 from metadata.services import (
-    GeneticFindingsSerializer, AnalyteSerializer, PhenotypeSerializer,
-    FamilySerializer, ParticipantInputSerializer, ParticipantOutputSerializer
+    GeneticFindingsSerializer,
+    AnalyteSerializer,
+    FamilySerializer,
+    ParticipantInputSerializer,
 )
 
-class FamilyModelTest(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
-
-    def test_family_creation(self):
-        family = Family.objects.first()
-        self.assertIsNotNone(family)
-        self.assertTrue(hasattr(family, 'family_id'))
-
-class ParticipantModelTest(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
-
-    def test_participant_creation(self):
-        participant = Participant.objects.first()
-        self.assertIsNotNone(participant)
-        self.assertTrue(hasattr(participant, 'participant_id'))
-
-    def test_participant_family_relationship(self):
-        participant = Participant.objects.first()
-        self.assertTrue(hasattr(participant, 'family_id'))
-        self.assertTrue(participant.family_id is None or isinstance(participant.family_id, Family))
-
-class PhenotypeModelTest(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
-
-    def test_phenotype_creation(self):
-        phenotype = Phenotype.objects.first()
-        self.assertIsNotNone(phenotype)
-        self.assertTrue(hasattr(phenotype, 'phenotype_id'))
-
-class GeneticFindingsModelTest(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
-
-    def test_genetic_findings_creation(self):
-        genetic_finding = GeneticFindings.objects.first()
-        self.assertIsNotNone(genetic_finding)
-        self.assertTrue(hasattr(genetic_finding, 'genetic_findings_id'))
-
-    def test_genetic_findings_parser(self):
-        test_data = {
-            "experiment_id": "EXP1|EXP2",
-            "pos": "100",
-            "allele_balance_or_heteroplasmy_percentage": "50",
-            "gene_of_interest": "GENE1|GENE2",
-            "method_of_discovery": "METHOD1|METHOD2",
-            "condition_inheritance": "INHERIT1|INHERIT2"
-        }
-        parsed_data = genetic_findings_parser(test_data)
-        
-        self.assertIsInstance(parsed_data["experiment_id"], list)
-        self.assertEqual(parsed_data["pos"], 100.0)
-        self.assertEqual(parsed_data["allele_balance_or_heteroplasmy_percentage"], 50)
-        self.assertIsInstance(parsed_data["gene_of_interest"], list)
-        self.assertIsInstance(parsed_data["method_of_discovery"], list)
-        self.assertIsInstance(parsed_data["condition_inheritance"], list)
-
-class AnalyteModelTest(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
-
-    def test_analyte_creation(self):
-        analyte = Analyte.objects.first()
-        self.assertIsNotNone(analyte)
-        self.assertTrue(hasattr(analyte, 'analyte_id'))
-
-    def test_analyte_participant_relationship(self):
-        analyte = Analyte.objects.first()
-        self.assertTrue(hasattr(analyte, 'participant_id'))
-        self.assertIsInstance(analyte.participant_id, Participant)
-
-class SelectorTests(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
-
-    def test_get_analyte(self):
-        analyte = Analyte.objects.first()
-        retrieved_analyte = get_analyte(analyte.analyte_id)
-        self.assertEqual(retrieved_analyte, analyte)
-
-    def test_get_analyte_not_found(self):
-        self.assertIsNone(get_analyte("nonexistent_id"))
-
-    def test_participant_parser(self):
-        test_data = {"twin_id": "T1|T2", "age_at_last_observation": "30", "age_at_enrollment": "25"}
-        parsed_data = participant_parser(test_data)
-        self.assertIsInstance(parsed_data["twin_id"], list)
-        self.assertEqual(parsed_data["age_at_last_observation"], 30.0)
-        self.assertEqual(parsed_data["age_at_enrollment"], 25.0)
 
 class ServicesTests(TestCase):
-    fixtures = ['tests/fixtures/test_fixture.json']
+    fixtures = ["tests/fixtures/test_fixture.json"]
 
     def test_genetic_findings_serializer_create(self):
         data = {
@@ -110,7 +28,7 @@ class ServicesTests(TestCase):
             "ref": "A",
             "alt": "T",
             "zygosity": "Homozygous",
-            "variant_inheritance": "biparental"
+            "variant_inheritance": "biparental",
         }
         serializer = GeneticFindingsSerializer(data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
@@ -122,7 +40,7 @@ class ServicesTests(TestCase):
             "analyte_id": "A001",
             "participant_id": Participant.objects.first().participant_id,
             "analyte_type": "DNA",
-            "primary_biosample": "UBERON:0000178"
+            "primary_biosample": "UBERON:0000178",
         }
         serializer = AnalyteSerializer(data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
@@ -141,9 +59,42 @@ class ServicesTests(TestCase):
             "participant_id": "P001",
             "consent_code": "HMB",
             "solve_status": "Unsolved",
-            "gregor_center": "UCI"
+            "gregor_center": "UCI",
         }
         serializer = ParticipantInputSerializer(data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
         instance = serializer.save()
         self.assertEqual(instance.participant_id, "P001")
+
+
+# class BioBanklTests(TestCase):
+#     # fixtures = ['tests/fixtures/test_fixture.json']
+#     fixtures = ['dump.json']
+
+#     def dump_test_data(self, file_name:str="test_results.json")-> None:
+#         out = StringIO()
+#         call_command('dumpdata', '--exclude', 'contenttypes', '--indent', '2', stdout=out)
+#         with open(file_name, 'w') as f:
+#             f.write(out.getvalue())
+
+#     def test_validate_biobank_traceability(self):
+#         for sample in Biobank.objects.all():
+#             result = validate_biobank_traceability(sample)
+#             if result["status"] == "invalid":
+#                 print(result)
+
+#     def fix_all_biobank_entries(self):
+#         results = {}
+
+#         orphaned_analytes = Analyte.objects.filter(biobank__isnull=True)
+#         homed_analytes = Analyte.objects.filter(biobank__isnull=False)
+#         import pdb; pdb.set_trace()
+#         # for analyte in orphaned_analytes:
+
+
+#     def full_fix_all_biobank_entries(self):
+# results = []
+# for biobank in Biobank.objects.all():
+#     result = repair_biobank_links(biobank)
+#     results.append(result)
+# return results
