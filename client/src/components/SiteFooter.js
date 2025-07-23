7a7a07817b399eadf23568680a90ddf9304480a6
Hadley King <HadleyKing@users.noreply.github.com>
2025-07-23 15:26:55 -0400

    Homepage (#151)
    * Cleaned up the homepage
    
    Changes to be committed:
    	modified:   client/.env.example
    	new file:   client/public/miaLogo192.png
    	modified:   client/src/App.css
    	modified:   client/src/components/GregorTables.js
    	new file:   client/src/components/SiteFooter.js
    	modified:   client/src/pages/Dashboard.js
    	modified:   client/src/pages/Home.js
    
    ---------
    
    Co-authored-by: IvantheDugtrio <idedios@usc.edu>

---
+++ client/src/components/SiteFooter.js
@@ -0,0 +1,68 @@
+//src/components/SiteFooter.js
+import { Footer } from "antd/es/layout/layout";
+import { Space, Tooltip } from "antd";
+import { ApiOutlined, GithubOutlined, CopyOutlined } from '@ant-design/icons';
+import { useLocation } from "react-router-dom";
+
+const APIDB = process.env.REACT_APP_APIDB;
+const GIT_VERSION = process.env.REACT_APP_VERSION
+
+const SiteFooter = ({ showSwagger = true, showGitHub = true }) => {
+  const APIDB = process.env.REACT_APP_APIDB;
+
+  return (
+    <Footer className="site-footer">
+      <Space >
+        <Tooltip title="UCI ICTS Dashboard">©2024 UCI</Tooltip>
+
+        {showSwagger && (
+          <Space>
+            <Tooltip title="Swagger API site">
+              <ApiOutlined />
+              <a
+                href={`${APIDB}api/swagger/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={(e) => e.stopPropagation()}
+              > Swagger API</a>
+            </Tooltip>
+            <Tooltip title="Redoc API docs">
+              <CopyOutlined />
+              <a
+                href={`${APIDB}api/redoc/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={(e) => e.stopPropagation()}
+              > Redoc API docs</a>
+            </Tooltip>
+          </Space>
+        )}
+
+        {showGitHub ? (
+          <Tooltip title="UCI ICTS Dashboard GitHub">
+            <GithubOutlined />
+            <a
+              href={GIT_VERSION}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => e.stopPropagation()}
+            > GitHub</a>
+          </Tooltip>
+        ) : (
+          <Tooltip title="UCI ICTS GitHub">
+            <GithubOutlined />
+            <a
+              href="https://github.com/UCI-ICTS"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => e.stopPropagation()}
+            > GitHub</a>
+          </Tooltip>
+        )}
+      </Space>
+    </Footer>
+  );
+};
+
+export default SiteFooter;
+
