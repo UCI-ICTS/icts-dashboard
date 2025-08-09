2fef0388459a056d778c971d9852038cab04ae79
IvantheDugtrio <idedios@usc.edu>
2025-08-08 18:33:13 -0700

    Edit fallback postgres username and password in settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -71,8 +71,8 @@ SHOW_ACTIVATION_LINKS = DEBUG
 # DATABASE settings
 ENGINE=secrets.get("DATABASE", "ENGINE", fallback="django.db.backends.postgresql")
 NAME=secrets.get("DATABASE", "NAME", fallback="ictsdashboard_app")
-USER=secrets.get("DATABASE", "USER", fallback="ictsdashboarduser")
-PASSWORD=secrets.get("DATABASE", "PASSWORD", fallback="ictsdashboarduser")
+USER=secrets.get("DATABASE", "USER", fallback="postgres")
+PASSWORD=secrets.get("DATABASE", "PASSWORD", fallback="postgres")
 HOST=secrets.get("DATABASE", "HOST", fallback="localhost") # Or your PostgreSQL server
 PORT=secrets.get("DATABASE", "PORT", fallback="5432") # Default PostgreSQL port
 
