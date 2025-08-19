683d7192539e285ca22098ec7bea5077babefb65
Hadley King <HadleyKing@users.noreply.github.com>
2025-08-19 17:55:26 -0400

    Update Specimine Type for biobank (#195)
    Changes to be committed:
    	modified:   client/src/components/SchemaForm.js
    	modified:   client/src/pages/GregorDataSheets.js
    	modified:   client/src/utils/schemaAndTables.js

--- client/src/utils/schemaAndTables.js
+++ client/src/utils/schemaAndTables.js
@@ -220,6 +220,16 @@ export const primaryBiosample = {
 	"UBERON:0001133": "cardiac tissue"
 }
 
+export const specimenType = {
+  "D": "EDTA in Cryovial",
+  "R": "PAX Tube",
+  "OG": "OGR-500 saliva collection kit",
+  "SC": "OCD-100 buccal collection kit",
+  "SG": "OGR-675 buccal collection kit",
+  "X": "Extracted DNA",
+  "XR": "Extracted RNA"
+}
+
 export const TABLE_MAPPING = [
   { name: "Participants", schema: "participants", identifier: "participant_id" },
   { name: "Families", schema: "families", identifier: "family_id" },
