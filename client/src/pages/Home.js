58cc2bd39b1895990c48b141c7768a9cd99f7390
HadleyKing <charles.hadley.king@gmail.com>
2025-08-20 10:06:17 -0400

    Home page changes
    Changes to be committed:
    	modified:   client/.env.example
    	new file:   client/public/geneyx-clean.svg
    	new file:   client/public/health-blue.png
    	new file:   client/public/udn_logo.png
    	modified:   client/src/App.css
    	modified:   client/src/pages/Home.js
    	new file:   client/src/utils/Home.js
    	new file:   server/dumpUpdate.json

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, Col, Row, Layout, Space, Typography } from 'antd';
+import { Card, Col, Row, Layout, Space, Typography, Popover } from 'antd';
 import SiteFooter from "../components/SiteFooter";
 import ReactMarkdown from 'react-markdown'
-
+import { home, home2 } from "../utils/Home";
 const { Content } = Layout;
 const { Title } = Typography;
 
@@ -15,97 +15,269 @@ const MIA = process.env.REACT_APP_MIA;
 const SNP = process.env.REACT_APP_SNP;
 
 const HomePage = () => {
-  const README_URL = "https://raw.githubusercontent.com/UCI-ICTS/icts-dashboard/refs/heads/dev/docs/HomePage.MD"
   const navigate = useNavigate();
-  const [summary, setSummary] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const fetchSummary = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get(README_URL);
-      setSummary(response.data);
-      setError(null);
-    } catch (err) {
-      console.error("Error loading summary:", err);
-      setError("Failed to load summary data.");
-    } finally {
-      setLoading(false);
-    }
-  };
+  const summary = home
+  const summary2 = home2
+  // const [summary, setSummary] = useState();
+  // const README_URL = "https://raw.githubusercontent.com/UCI-ICTS/icts-dashboard/refs/heads/dev/docs/HomePage.MD"
+  // const fetchSummary = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await axios.get(README_URL);
+  //     setSummary(response.data);
+  //     setError(null);
+  //   } catch (err) {
+  //     console.error("Error loading summary:", err);
+  //     setError("Failed to load summary data.");
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
   
-  useEffect(() => {
-    fetchSummary();
-  }, []);
+  // useEffect(() => {
+  //   fetchSummary();
+  // }, []);
 
   return (
     <Layout className="fullscreen-bg">
         <Title level={1} className='site-title'>UCI Institute for Clinical & Translational Science (ICTS) </Title>
         <Content className="site-content">
-          <Row gutter={[16, 16]}>
-            <Col xs={24} md={8}>
-              <Card
-                hoverable
-                className="primary-card"
-                onClick={()=> navigate("/dashboard")}
-                title={
-                  <Space>
-                    <span className="card-title">ICTS Dashboard</span>
+          <Row className="grid-row">
+            <Card className="primary-card">
+              <ReactMarkdown
+                // Make links open in new tab safely
+                components={{
+                  a: ({ node, ...props }) => (
+                    <a {...props} target="_blank" rel="noopener noreferrer" />
+                  ),
+                  img: ({ node, ...props }) => (
+                    <img {...props} style={{ maxWidth: "100%" }} alt={props.alt || ""} />
+                  ),
+                }}
+              >
+                {summary}
+              </ReactMarkdown>
+            </Card>
+          </Row>
+          {/* Resources */}
+          <Title level={0} className='site-subtitle'>Resources </Title>
+          <Row gutter={[16, 16]} className="cards-equal-row" >
+            <Col className="card-col flex-col">
+              <Popover
+                overlayClassName="themed-popover"
+                title="ICTS Dashboard"
+                content={<div className="card-label">An application for clinicians, technicians, and informaticians
+                  to interact with the UCI site GREGoR data</div>}
+              >
+                <Card
+                  hoverable
+                  className="primary-card card-fill"
+                  onClick={()=> navigate("/dashboard")}
+                >
+                  <span className="home-card">ICTS Dashboard</span> 
+                  <span className="card-label">by</span> 
+                  <img
+                      src="/health-blue.png"
+                      alt="UCI Health logo"
+                      style={{
+                        width: 130,
+                        objectFit: "contain",
+                        display: "inline-block"
+                      }}
+                    />
+                </Card>
+              </Popover>
+            </Col>
+            <Col className="card-col flex-col">
+              <Popover
+                overlayClassName="themed-popover"
+                title="GREGoR Data Model"
+                content={
+                  <span className="card-label">
+                    The Consortium Data Model has been designed to reflect the relationships between families, 
+                    participants, and molecular sequencing data in the GREGoR Consortium. It is expanded over
+                    time through a process of versioned releases.
+                  </span>}
+                >
+                  <Card
+                    hoverable
+                    className="primary-card"
+                    onClick={()=> {window.open("https://github.com/UW-GAC/gregor_data_models?tab=readme-ov-file", "_blank")}}
+                  >
+                    <span className="home-card">GREGoR Data Model</span>
+                    <div/>
+                    <img
+                      src="/GREGoR_Final_Logo.png"
+                      alt="GREGoR logo"
+                      style={{
+                        width: 130,
+                        objectFit: "contain",
+                        display: "inline-block"
+                      }}
+                    />
+                  </Card>
+              </Popover>
+            </Col>
+            <Col className="card-col flex-col">
+              <Popover
+              overlayClassName="themed-popover"
+              title="Medical Information Assistant (MIA)"
+              content={
+                <span className="card-label">
+                  Our virtual Medical Information Assistant (Mia)
+                  A consentbot that facilitates virtual conversations with patients.
+                </span>
+              }>
+                <Card
+                  hoverable
+                  className="primary-card card-fill"
+                  onClick={()=> {window.open(`${MIA}`, "_blank")}}
+                >
+                  <img
+                    src="/miaLogo192.png"
+                    alt="MIA logo"
+                    style={{
+                      width: 60,
+                      objectFit: "contain",
+                      display: "inline-block"
+                    }}
+                  />
+                  <span className="home-card">Medical Information Assistant (MIA)</span>
+                    
+                </Card>
+              </Popover>
+            </Col>
+            <Col className="card-col flex-col">
+              <Popover
+                overlayClassName="themed-popover"
+                title="Geneyx"
+                content={
+                  <span className="card-label">
+                    NGS data analysis and interpretation platform.
+                  </span>}
+                >
+                  <Card
+                    hoverable
+                    className="primary-card"
+                    onClick={()=> {window.open("https://analysis.geneyx.com/account/logon", "_blank")}}
+                  >
+                    <img
+                      src="/geneyx-clean.svg"
+                      alt="geneyx"
+                      style={{
+                        // width: 130,
+                        height: 70,
+                        background: "black",
+                        objectFit: "contain",
+                        display: "inline-block"
+                      }}
+                    />
+                    <br/>
+                    <span className="home-card">Geneyx</span>
+                  </Card>
+              </Popover>
+            </Col>
+          </Row>
+        {/* Collabs */}
+          <Title level={0} className='site-subtitle'>Collaborations</Title>
+            <Row gutter={[16, 16]} className="cards-equal-row" >
+              <Col className="card-col flex-col">
+                <Popover
+                  overlayClassName="themed-popover"
+                  title="The GREGoR Consortium"
+                  content={<div className="card-label">The GREGoR Consortium (Genomics Research to Elucidate
+                    the Genetics of Rare diseases) seeks to develop and apply approaches to discover the
+                    cause of currently unexplained rare genetic disorders.</div>}
+                >
+                  <Card
+                    hoverable
+                    className="primary-card card-fill"
+                    onClick={()=> {window.open("https://gregorconsortium.org/", "_blank")}}
+                  >
+                    
+                      <span className="home-card">The GREGoR Consortium</span>
                       <img
                         src="/GREGoR_Final_Logo.png"
-                        alt="GREGoR"
+                        alt="GREGoR logo"
                         style={{
                           width: 130,
                           objectFit: "contain",
                           display: "inline-block"
                         }}
                       />
-                  </Space>
-              }>
-              <span className="card-label">The GREGoR Consortium (Genomics Research to Elucidate the Genetics of Rare diseases) seeks to develop and apply approaches to discover the cause of currently unexplained rare genetic disorders.</span>
-              </Card>
-            </Col>
-            <Col xs={24} md={8}>
-              <Card
-                hoverable
-                className="primary-card"
-                onClick={()=> {window.open(`${SNP}`, "_blank")}}
-                title={<span className="card-title">SNP Consortium Archive</span>}
-              ><span className="card-label">Landing page and interface for the SNP Consortium archival material.</span></Card>
-            </Col>
-            <Col xs={24} md={8}>
-              <Card
-                hoverable
-                className="primary-card"
-                onClick={()=> {window.open(`${MIA}`, "_blank")}}
-                title={
-                  <Space>
+                    
+                  </Card>
+                </Popover>
+              </Col>
+              <Col className="card-col flex-col">
+                <Popover
+                  overlayClassName="themed-popover"
+                  title="SNP Consortium Archive"
+                  content={
+                    <span className="card-label">
+                      Landing page and interface for the SNP Consortium archival material.
+                    </span>}
+                  >
+                    <Card
+                      hoverable
+                      className="primary-card card-fill"
+                      onClick={()=> {window.open(`${SNP}`, "_blank")}}
+                    >
+                      <span className="home-card">SNP Consortium Archive</span>
+                    </Card>
+                </Popover>
+              </Col>
+              <Col className="card-col flex-col">
+                <Popover
+                overlayClassName="themed-popover"
+                title="Undiagnosed Diseases Network (UDN)"
+                content={
+                  <span className="card-label">
+                    The Undiagnosed Diseases Network (UDN) is a research study backed by
+                    the National Institutes of Health that seeks to provide answers for 
+                    patients and families affected by these mysterious conditions.
+                  </span>
+                }>
+                  <Card
+                    hoverable
+                    className="primary-card card-fill"
+                    onClick={()=> {window.open("https://undiagnosed.hms.harvard.edu/", "_blank")}}
+                  >
+                    <span className="home-card">Undiagnosed Diseases Network (UDN)</span>
                     <img
-                      src="/miaLogo192.png"
-                      alt="MIA logo"
+                      src="/udn_logo.png"
+                      alt="UDN logo"
                       style={{
-                        width: 30,
+                        width: 130,
                         objectFit: "contain",
                         display: "inline-block"
                       }}
                     />
-                    <span className="card-title">Medical Information Assistant (MIA)</span>
-                  </Space>}
-              ><span className="card-label">Our virtual Medical Information Assistant (Mia)
-                A consentbot that facilitates virtual conversations with patients.</span>
-              </Card>
-            </Col>
-            <Col xs={24} md={8}>
-            </Col>
-            <Col xs={24} md={8}>
-            </Col>
+                  </Card>
+                </Popover>
+              </Col>
             </Row>
-            <Row gutter={[16, 16]}>
+            {/* Other Content */}
+            <Row className="grid-row">
               <Card className="primary-card">
-                <ReactMarkdown>{summary}</ReactMarkdown>
+                <ReactMarkdown
+                  // Make links open in new tab safely
+                  components={{
+                    a: ({ node, ...props }) => (
+                      <a {...props} target="_blank" rel="noopener noreferrer" />
+                    ),
+                    img: ({ node, ...props }) => (
+                      <img {...props} style={{ maxWidth: "100%" }} alt={props.alt || ""} />
+                    ),
+                  }}
+                >
+                  {summary2}
+                </ReactMarkdown>
               </Card>
-            </Row>
+          </Row>
+
         </Content>
         <SiteFooter showSwagger={false} showGitHub={false} />
     </Layout>
