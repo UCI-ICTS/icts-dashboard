7d5d6d076dd12b997ea55dcf8a1d7d8611c88f24
IvantheDugtrio <idedios@usc.edu>
2025-08-20 14:27:15 -0700

    Shows ancestor nodes now

--- server/utilities/ols.py
+++ server/utilities/ols.py
@@ -18,7 +18,7 @@ def get_args():
     parser.add_argument(
         '-t', '--term',
         required=True,
-        help='term id (e.g. HP:0000017, MONDO:000547, OMIM:, etc.)'
+        help='term id (e.g. HP:0000017, MONDO:000547, ORPHA:, etc.)'
     )
     return parser.parse_args()
 
@@ -30,21 +30,70 @@ def query_ols(term_id):
         headers={'accept':'*/*'})
     assert health_response.ok
 
-    if term_id.startswith('ORPHA'):
-        term_id = re.sub('ORPHA', 'ORDO', term_id)
-    term_id = urllib.parse.quote(term_id)  # Format term to url
+    # Ontology IDs and IRIs
+    onto_lookup = {
+        "HP": "http://purl.obolibrary.org/obo/",
+        "MONDO": "http://purl.obolibrary.org/obo/",
+        "ORDO": "http://www.orpha.net/ORDO/",
+        "SNOMED": "http://snomed.info/id/",
+    }
+
+    term_id = re.sub(':', '_', term_id)  # Short-form name, no colon
+    onto = term_id.split('_')[0]
+    if onto == "ORPHA":
+        term_id = re.sub("ORPHA", "Orphanet", term_id).strip()
+        onto = "ORDO"
+    elif onto == "SNOMED":
+        term_id = re.sub("SNOMED_", "", term_id)
+    iri = onto_lookup[onto] + term_id
+    iri = urllib.parse.quote(iri, safe='', encoding='utf-8', errors='strict')  # double URL encoding
+    iri = urllib.parse.quote(iri, safe='', encoding='utf-8', errors='strict')  # double URL encoding
+    request_url = f"{EBI_OLS_API}/ontologies/{onto}/terms/{iri}?lang={LANG}"
+    ancestors_url = f"{EBI_OLS_API}/ontologies/{onto}/terms/{iri}/ancestors?lang={LANG}"
+    print(request_url)
     term_response = requests.get(
-        url=f"{EBI_OLS_API}/terms?obo_id={term_id}&lang={LANG}",
-        headers={'accept':'application/json'})
-    if term_response.ok:
-        json_response = json.loads(term_response.content)
-        last_term_desc = ""  # dedup responses
-        #import pdb; pdb.set_trace()
-        for term in json_response['_embedded']['terms']:
-            if last_term_desc == term['description']:
-                continue
-            print(term['label'], term['description'])
-            last_term_desc = term['description']
+        url=request_url,
+        headers={'accept':'application/json'}
+    )
+    assert term_response.ok
+    ancestors_response = requests.get(
+        url=ancestors_url,
+        headers={'accept':'application/json'}
+    )
+    assert ancestors_response.ok
+
+    term_json = json.loads(term_response.content)
+    ancestors_json = json.loads(ancestors_response.content)
+
+    print(f"obo_id: {term_json['obo_id']}")
+    print(f"label: {term_json['label']}")
+    print(f"synonyms: {term_json['synonyms']}")
+    print(f"cross reference: {term_json['annotation']['database_cross_reference']}")
+
+    nodes_ignore = [  # List of nodes to ignore from MONDO, HP, and ORDO
+        "entity",
+        "continuant",
+        "specifically dependent continuant",
+        "realizable entity",
+        "disposition",
+        "disease",
+        "human disease",
+        "All",
+        "Phenotypic abnormality",
+        "clinical entity",
+        "Disease",
+        "disorder",
+    ]
+
+    if "_embedded" in ancestors_json:
+        if "terms" in ancestors_json["_embedded"]:
+            print("\nAncestor nodes:")
+            tab_counter = ""
+            for term in ancestors_json["_embedded"]["terms"][::-1]:
+                if term["label"] in nodes_ignore:
+                    continue
+                print(f"{tab_counter}- {term['label']}")
+                tab_counter += " "
 
 
 if __name__ == "__main__":
