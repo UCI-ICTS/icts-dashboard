69f948c557b32bcecdfc4b200994d181473414e8
Hadley King <HadleyKing@users.noreply.github.com>
2025-08-25 12:42:54 -0400

    Uploader docs (#200)
    * Add GeneticFiningsInputSerializer
    - implemented cross-checks that are not in JSON
    
    Changes to be committed:
    	modified:   server/metadata/apis.py
    	modified:   server/metadata/models.py
    	modified:   server/metadata/services.py
    	modified:   server/search/selectors.py
    	modified:   server/submodels/models.py
    	modified:   server/tests/test_apis/test_genetic_findings_apis.py
    	modified:   server/tests/test_apps/test_metadata/test_services.py
    
    * Conditional Validators added to SchemaForm for UI
    
    Changes to be committed:
    	modified:   client/src/components/SchemaForm.js
    	modified:   client/src/pages/GregorDataSheets.js
    	modified:   client/src/utils/schemaAndTables.js
    
    * Finish genetic findings validation additions
    
    Changes to be committed:
    	modified:   client/src/App.css
    	modified:   client/src/components/SchemaForm.js
    	modified:   client/src/pages/Home.js
    	modified:   client/src/utils/schemaAndTables.js
    	modified:   server/metadata/apis.py
    	modified:   server/metadata/selectors.py
    	modified:   server/metadata/services.py
    	modified:   server/submodels/models.py
    
    * Uploader Docs
    
    Changes to be committed:
    	deleted:    client/src/api.js
    	modified:   client/src/services/account.service.js
    	modified:   client/src/services/data.service.js
    	modified:   client/src/services/error.service.js
    	modified:   client/src/slices/dataSlice.js
    	modified:   client/src/utils/axiosConfig.js

--- client/src/services/data.service.js
+++ client/src/services/data.service.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { store } from "../store";
-import api from "../api";
+import api from "../utils/axiosConfig";
 
 
 const APIDB = process.env.REACT_APP_APIDB;
@@ -23,55 +23,55 @@ const getAuthHeaders = () => {
 };
 
 const getAllTables = async () => {
-  const response = await axios.get(`${APIDB}api/search/get_all_tables/`, {
+  const response = api.get(`${APIDB}api/search/get_all_tables/`, {
     headers: getAuthHeaders(),
   });
   return response;
 }
 
 const familyDetail = async (participant_id) => {
-    const response = await axios.get(APIDB + `api/search/family_detail/?ids=${participant_id}`, {headers: getAuthHeaders()})
+    const response = api.get(APIDB + `api/search/family_detail/?ids=${participant_id}`, {headers: getAuthHeaders()})
     return response
 }
 
 const fetchTable = async (table) => {
   if (metadata.includes(table)) {
-    const response = await axios.get(APIDB + `api/metadata/${table}/all/`, {headers: getAuthHeaders()})
+    const response = api.get(APIDB + `api/metadata/${table}/all/`, {headers: getAuthHeaders()})
     return response
   } else {
-    const response = await axios.get(APIDB + `api/experiments/${table}/all/`, {headers: getAuthHeaders()})
+    const response = api.get(APIDB + `api/experiments/${table}/all/`, {headers: getAuthHeaders()})
     return response
   }
 }
 
 const createEntry = async (table, data) => {
   if (metadata.includes(table)) {
-    const response = await axios.post(APIDB + `api/metadata/${table}/create/`, data, {headers: getAuthHeaders()})
+    const response = api.post(APIDB + `api/metadata/${table}/create/`, data, {headers: getAuthHeaders()})
     return response
   } else {
-    const response = await axios.post(APIDB + `api/experiments/${table}/create/`, data, {headers: getAuthHeaders()})
+    const response = api.post(APIDB + `api/experiments/${table}/create/`, data, {headers: getAuthHeaders()})
     return response
   }
 }
 
 const updateEntry = async (table, data) => {
   if (metadata.includes(table)) {
-    const response = await axios.post(APIDB + `api/metadata/${table}/update/`, data, {headers: getAuthHeaders()})
+    const response = api.post(APIDB + `api/metadata/${table}/update/`, data, {headers: getAuthHeaders()})
     return response
   } else {
-    const response = await axios.post(APIDB + `api/experiments/${table}/update/`, data, {headers: getAuthHeaders()})
+    const response = api.post(APIDB + `api/experiments/${table}/update/`, data, {headers: getAuthHeaders()})
     return response
   }
 }
 
 const deleteEntry = async (table, idList) => {
   if (metadata.includes(table)) {
-    const response = await axios.delete(APIDB + `api/metadata/${table}/delete/?ids=${idList}`, {
+    const response = api.delete(APIDB + `api/metadata/${table}/delete/?ids=${idList}`, {
       headers: getAuthHeaders()
     })
     return response
   } else {
-    const response = await axios.delete(APIDB + `api/experiments/${table}/delete/?ids=${idList}`, {
+    const response = api.delete(APIDB + `api/experiments/${table}/delete/?ids=${idList}`, {
       headers: getAuthHeaders()
     })
     return response
